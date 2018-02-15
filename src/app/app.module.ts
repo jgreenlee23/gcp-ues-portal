c5dd7d34d7ada6c22a09d1ac4064ca5206681cd0
Justin Greenlee <justin.greenlee@Justins-MacBook-Pro.local>
2018-02-15 13:01:02 -0600

    Added some code...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { BigQueryService } from './bigquery.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [BigQueryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
