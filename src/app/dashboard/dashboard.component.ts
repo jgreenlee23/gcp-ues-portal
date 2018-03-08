cb80730f1e2ebed816256bce6690c0410f60fa93
Justin Greenlee <justin.greenlee@masergy.com>
2018-03-08 16:17:26 -0600

    Added drag-and-drop functionality to CSS Grid.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BigQueryService } from '../bigquery.service';
-declare var jquery:any;
-declare var $ :any;
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'dashboard',
@@ -9,58 +9,139 @@ declare var $ :any;
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  
+
+  elemPositionX: 0;
+  elemPositionY: 0;
+  elemHeight: 0;
+  elemWidth: 0;
+  isMouseDown: false;
+  myTimer: 0;
+  mousePosX: 0;
+  mousePosY: 0;
+
   constructor() {
-    
+
   }
-  
+
   ngOnInit() {
-    // this.getDatasets();
-    // $('.wrapper > div').resizable();
-    // var element = document.getElementById('element');
-    // var resizer = document.createElement('div');
-    // resizer.className = 'resizer';
-    // resizer.style.width = '10px';
-    // resizer.style.height = '10px';
-    // resizer.style.background = 'red';
-    // resizer.style.position = 'absolute';
-    // resizer.style.right = 0;
-    // resizer.style.bottom = 0;
-    // resizer.style.cursor = 'se-resize';
-    // element.appendChild(resizer);
-    // resizer.addEventListener('mousedown', initResize, false);
+
   }
-  
+
   onDragStart(event: any) {
-    // event.dataTransfer.setData('data', data);
     console.log("hello world..");
+    console.log(event);
+    
+    // TODO: stylize element being dropped
+    
+    // var crt = $(event.target).cloneNode(true);
+    // crt.style.backgroundColor = "red";
+    // crt.style.display = "none"; /* or visibility: hidden, or any of the above */
+    // document.body.appendChild(crt);
+    // event.dataTransfer.setDragImage($(event.target), 0, 0);
   }
   
+  computeGrid() {
+    // TODO: loop through grid-components in wrapper; create serialiable JSON data representing grid
+  }
+
   onDragOver(event: any) {
     console.log(event);
     console.log("dragging over...");
-  }
-  
-  onDrop(event: any) {
-    // let dataTransfer = event.dataTransfer.getData('data');
-    // event.preventDefault();
-    console.log("hello drop..");
-  }
-  
-  onClickMe(event: any) {
-    console.log("testing...");
+    
+    // event.target.css("grid-column");
+    // event.target.css("grid-row");
+    
+    // TODO: get grid-column and grid-row from computeGrid array for event.target
+    // TODO: create placeholder grid-component with dimensions (column x row) of event.target
+    // TODO: remove previous grid-component (the one being dropped) to correctly show how components will be relocated
+    
   }
 
-  // initResize(event: any) {
-  //    window.addEventListener('mousemove', Resize, false);
-  //    window.addEventListener('mouseup', stopResize, false);
-  // }
-  // resize(event: any) {
-  //    element.style.width = (e.clientX - element.offsetLeft) + 'px';
-  //    element.style.height = (e.clientY - element.offsetTop) + 'px';
-  // }
-  // stopResize(event: any) {
-  //     window.removeEventListener('mousemove', Resize, false);
-  //     window.removeEventListener('mouseup', stopResize, false);
-  // }
+  onDrop(event: any) {
+    // event.preventDefault();
+    console.log("hello drop..");
+    
+    // TODO: get placeholder grid-component dimensions
+    // TODO: place component being dropped 
+  }
+
+  startMove() {
+    var $target = $(event.target);
+
+    // Set position mouse clicked in element for tracking //
+    this.elemPositionX = event.pageX - $target.offset().left;
+    this.elemPositionY = event.pageY - $target.offset().top;
+    this.isMouseDown = true;
+
+    // disable element highlighting
+    $("body").children().css("user-select", "none");
+
+    // Set margin next to sibling so open space where item will currently go //
+    // if ($target.next().length) {
+    //   $target.next().css("margin-left", $target.width());
+    // } else {
+    //   $target.prev().css("margin-left", $target.width());
+    // }
+
+    $('.wrapper').removeClass('edit');
+    $target.addClass("moving");
+    this.elemWidth = $target.width();
+    this.elemHeight = $target.height();
+    
+    document.addEventListener("mouseup", this.finishMovingItem.bind(this), false);
+    document.addEventListener('mousemove', this.onMouseUpdate.bind(this), false);
+    document.addEventListener('mouseenter', this.onMouseUpdate.bind(this), false);
+    
+    this.myTimer = setInterval(function() {
+      if (this.isMouseDown) {
+    
+        // set moving item to track mouse movement
+        $('.moving').css({
+          "position": "absolute",
+          "left": (this.mousePosX - $(window).scrollLeft() - this.elemPositionX) + "px",
+          "top": (this.mousePosY - $(window).scrollTop() - this.elemPositionY) + "px",
+          "height": this.elemHeight,
+          "width": this.elemWidth,
+          // "box-shadow": "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"
+        });
+        // Set tracking over sibling elements
+        // $target.siblings(".movable").on("mouseover", function(siblingEvent){
+        //     var siblingMiddle = ($(this).width() + parseInt($(this).css("padding-right")) + parseInt($(this).css("padding-left")) + 2) / 2;
+        // 
+        //     $(this).siblings(":not('.moving')").removeAttr("style");
+        //     if(siblingEvent.offsetX > siblingMiddle){
+        //         self.model.set("moveItemTo", {$sibling: $(this), "position": "after"});
+        //         $(this).css("margin-right", $target.width());
+        //         $(this).css("margin-left", "10px");
+        //     } else {
+        //         self.model.set("moveItemTo", {$sibling: $(this), "position": "before"});
+        //         $(this).css("margin-left", $target.width());
+        //         $(this).css("margin-right", "10px");
+        //     }
+        // });
+    
+      }
+    }.bind(this), 10);
+  }
+  
+  finishMovingItem() {
+    document.removeEventListener("mouseup", this.finishMovingItem, false);
+    document.removeEventListener('mousemove', this.onMouseUpdate, false);
+    document.removeEventListener('mouseenter', this.onMouseUpdate, false);
+    clearInterval(this.myTimer);
+    this.mouseDown = false;
+  }
+
+  moveItem(event: any) {
+    var $target = this.model.get("$targetEl");
+
+    // set position of moving item  based on mouse position //
+    $target.css("left", (event.pageX - $(window).scrollLeft() - this.model.get("elemPositionX")) + "px");
+    $target.css("top", (event.pageY - $(window).scrollTop() - this.model.get("elemPositionY")) + "px");
+  },
+
+  onMouseUpdate(e) {
+      this.mousePosX = e.pageX;
+      this.mousePosY = e.pageY;
+  }
 }
