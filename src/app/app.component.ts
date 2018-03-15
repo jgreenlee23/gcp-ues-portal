4283b1e6bb3e5c57a1ce73baaab5c5351731ca6c
Justin Greenlee <justin.greenlee@masergy.com>
2018-03-15 11:01:56 -0500

    Added Angular Tour of Heroes to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+// import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [
-    './app.component.scss'
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  // 
+  // goBack(): void {
+  //   this.location.back();
+  // }
 }
